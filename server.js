1fa5c9339af1ccf93de1600a28139a7a259c011a
Kathy <kath.scholle@gmail.com>
2023-01-07 11:45:33 -0500

    wip: organize data and render template

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ console.log(process.env)
 const port = process.env.PORT; //PORT is an environment variable
 
 //configure database
-const fruits = ['apple', 'banana', 'pear'];
+const fruits = require('./models/fruits'); //tells it the file path
+console.log(fruits);
 
 //mount middleware
 
@@ -22,14 +23,25 @@ const fruits = ['apple', 'banana', 'pear'];
 
 //index - GET /fruits
 app.get('/fruits', function(req, res) {
+    const readyToEat = req.query.readyToEat;
+    console.log(readyToEat);
+    if(readyToEat) {
+        const filteredFruits = fruits.filter(function(f){
+            return f.readyToEat === (readyToEat === 'true') //true or false
+        });
+        res.send(filteredFruits);
+    } else {
+        res.send(fruits);
+    }
     res.send(fruits); //this is an express method
 })
 
 //show - GET /fruits/:someUniqueIdentifier
 app.get('/fruits/:index', function(req, res){
     const fruit = fruits[req.params.index];
-    res.send(fruit);
-})
+    res.render('show.ejs', {fruit: fruit});//invokes the function of the view engine, look into ejs files and turn it into html in the browser
+}); //1st fruit is key; 2nd fruit is value, it represents line 41; the name fruit is just to be more descriptive
+//{fruitInfo: fruit} is the data that it is passing, object allows us to hold collections of value
 
 //tell the app to listen on a dedicated port for requests
 app.listen(port, function(){
